c4daae971dbee6f648f2e495b0fc8e746925926c
hiteshi10 <79038973+hiteshi10@users.noreply.github.com>
2022-04-12 07:17:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const navBtn = document.getElementById('nav-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const sideNav = document.getElementById('sidenav');
+const modal = document.getElementById('modal');
+
+navBtn.addEventListener("click", function(){
+    sideNav.classList.add('show');
+    modal.classList.add('showModal');
+});
+
+cancelBtn.addEventListener('click', function(){
+    sideNav.classList.remove('show');
+    modal.classList.remove('showModal');
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        sideNav.classList.remove('show');
+        modal.classList.remove('showModal');
+    }
+});
+
+
+
+
+
+
+(function () {
+    "use strict";
+  
+    var carousels = function () {
+      $(".owl-carousel1").owlCarousel({
+        loop: true,
+        center: true,
+        margin: 0,
+        responsiveClass: true,
+        nav: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          680: {
+            items: 2,
+            nav: false,
+            loop: false
+          },
+          1000: {
+            items: 3,
+            nav: true
+          }
+        }
+      });
+    };
+  
+    (function ($) {
+      carousels();
+    })(jQuery);
+  })();
+
+
